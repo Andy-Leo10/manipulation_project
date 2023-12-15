ee88e6b6a6ac660523683d171696f5ea6172abb9
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2023-12-15 03:25:43 +0000

    Code was structured in a Class

--- moveit2_scripts/src/pick_and_place.cpp
+++ moveit2_scripts/src/pick_and_place.cpp
@@ -5,191 +5,161 @@
 #include <moveit_msgs/msg/display_trajectory.hpp>
 
 static const rclcpp::Logger LOGGER = rclcpp::get_logger("move_group_demo");
+static const std::string PLANNING_GROUP_ARM = "ur_manipulator";
+static const std::string PLANNING_GROUP_GRIPPER = "gripper";
+
+class RobotArm {
+public:
+    RobotArm() {
+        rclcpp::NodeOptions node_options;
+        node_options.automatically_declare_parameters_from_overrides(true);
+        move_group_node = rclcpp::Node::make_shared("move_group_interface_tutorial", node_options);
+
+        executor.add_node(move_group_node);
+        std::thread([this]() { this->executor.spin(); }).detach();
+
+        move_group_arm = std::make_shared<moveit::planning_interface::MoveGroupInterface>(move_group_node, PLANNING_GROUP_ARM);
+        joint_model_group_arm = move_group_arm->getCurrentState()->getJointModelGroup(PLANNING_GROUP_ARM);
+        
+        move_group_gripper = std::make_shared<moveit::planning_interface::MoveGroupInterface>(move_group_node, PLANNING_GROUP_GRIPPER);
+        joint_model_group_gripper = move_group_gripper->getCurrentState()->getJointModelGroup(PLANNING_GROUP_GRIPPER);
+
+        RCLCPP_INFO(LOGGER, "---------------------------INSTANTIATED!");
+    }
+
+    void move_joint_space(float angle_0, float angle_1, float angle_2, float angle_3, float angle_4, float angle_5) {
+        moveit::core::RobotStatePtr current_state = move_group_arm->getCurrentState(10);
+
+        std::vector<double> joint_group_positions;
+        current_state->copyJointGroupPositions(joint_model_group_arm, joint_group_positions);
+
+        joint_group_positions[0] = angle_0; // Shoulder Pan
+        joint_group_positions[1] = angle_1; // Shoulder Lift
+        joint_group_positions[2] = angle_2; // Elbow
+        joint_group_positions[3] = angle_3; // Wrist 1
+        joint_group_positions[4] = angle_4; // Wrist 2
+        joint_group_positions[5] = angle_5; // Wrist 3
+
+        move_group_arm->setJointValueTarget(joint_group_positions);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+        bool success = (move_group_arm->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if(success) {
+            move_group_arm->execute(my_plan);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Joint space movement plan failed!");
+        }
+    }
+
+    void move_end_effector(double x, double y, double z, double roll_deg, double pitch_deg, double yaw_deg) {
+        geometry_msgs::msg::Pose target_pose;
+        tf2::Quaternion q;
+        q.setRPY(roll_deg * M_PI/180.0, pitch_deg * M_PI/180.0, yaw_deg * M_PI/180.0);
+        target_pose.orientation.x = q.x();
+        target_pose.orientation.y = q.y();
+        target_pose.orientation.z = q.z();
+        target_pose.orientation.w = q.w();
+        target_pose.position.x = x;
+        target_pose.position.y = y;
+        target_pose.position.z = z;
+        move_group_arm->setPoseTarget(target_pose);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+
+        bool success = (move_group_arm->plan(my_plan) == moveit::core::MoveItErrorCode::SUCCESS);
+        if(success) {
+            move_group_arm->execute(my_plan);
+        } else {
+            RCLCPP_ERROR(LOGGER, "End effector movement plan failed!");
+        }
+    }
+
+    void move_waypoint(double delta, const std::string& direction) {
+        RCLCPP_INFO(LOGGER, "Move waypoint!");
+
+        std::vector<geometry_msgs::msg::Pose> waypoints;
+        geometry_msgs::msg::Pose target_pose = move_group_arm->getCurrentPose().pose;
+
+        if(direction == "x") {
+            target_pose.position.x += delta;
+        } else if(direction == "y") {
+            target_pose.position.y += delta;
+        } else if(direction == "z") {
+            target_pose.position.z += delta;
+        } else {
+            RCLCPP_ERROR(LOGGER, "Invalid direction!");
+            return;
+        }
+
+        waypoints.push_back(target_pose);
+
+        moveit_msgs::msg::RobotTrajectory trajectory;
+        const double jump_threshold = 0.0;
+        const double eef_step = 0.01;
+
+        double fraction = move_group_arm->computeCartesianPath(waypoints, eef_step, jump_threshold, trajectory);
+
+        if(fraction >= 0.0) {
+            move_group_arm->execute(trajectory);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Waypoint movement plan failed!");
+        }
+    }
+
+    void cmd_arm(const std::string& target_name) {
+        RCLCPP_INFO(LOGGER, target_name.c_str());
+
+        move_group_arm->setNamedTarget(target_name);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
+        bool success_arm = (move_group_arm->plan(my_plan_arm) == moveit::core::MoveItErrorCode::SUCCESS);
+
+        if(success_arm) {
+            move_group_arm->execute(my_plan_arm);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Arm plan failed!");
+        }
+    }
+
+    void cmd_gripper(const std::string& target_name) {
+        RCLCPP_INFO(LOGGER, target_name.c_str());
+
+        move_group_gripper->setNamedTarget(target_name);
+
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
+        bool success_gripper = (move_group_gripper->plan(my_plan_gripper) == moveit::core::MoveItErrorCode::SUCCESS);
+
+        if(success_gripper) {
+            move_group_gripper->execute(my_plan_gripper);
+        } else {
+            RCLCPP_ERROR(LOGGER, "Gripper plan failed!");
+        }
+    }
+
+private:
+    rclcpp::Node::SharedPtr move_group_node;
+    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_arm;
+    const moveit::core::JointModelGroup* joint_model_group_arm;
+    std::shared_ptr<moveit::planning_interface::MoveGroupInterface> move_group_gripper;
+    const moveit::core::JointModelGroup* joint_model_group_gripper;
+    rclcpp::executors::SingleThreadedExecutor executor;
+};
 
 int main(int argc, char **argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::NodeOptions node_options;
-  node_options.automatically_declare_parameters_from_overrides(true);
-  auto move_group_node =
-      rclcpp::Node::make_shared("move_group_interface_tutorial", node_options);
+    rclcpp::init(argc, argv);
+    RobotArm robotArm;
 
-  rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(move_group_node);
-  std::thread([&executor]() { executor.spin(); }).detach();
+    robotArm.cmd_arm("home");
+    robotArm.cmd_gripper("gripper_open");
+    // Shoulder Pan | Shoulder Lift | Elbow | Wrist 1 | Wrist 2 | Wrist 3
+    robotArm.move_joint_space(3.47, -1.64, -1.59, -1.47, 1.56, -5.70);
+    robotArm.cmd_gripper("gripper_close");
+    // robotArm.move_end_effector(0.2, -0.34, 0.28, 180.0, 0.0, 0.0);
+    robotArm.move_waypoint(-0.1, "z");
+    robotArm.move_waypoint(0.1, "x");
+    robotArm.move_waypoint(0.2, "y");
 
-  static const std::string PLANNING_GROUP_ARM = "ur_manipulator";
-  static const std::string PLANNING_GROUP_GRIPPER = "gripper";
-
-  moveit::planning_interface::MoveGroupInterface move_group_arm(
-      move_group_node, PLANNING_GROUP_ARM);
-  moveit::planning_interface::MoveGroupInterface move_group_gripper(
-      move_group_node, PLANNING_GROUP_GRIPPER);
-
-  const moveit::core::JointModelGroup *joint_model_group_arm =
-      move_group_arm.getCurrentState()->getJointModelGroup(PLANNING_GROUP_ARM);
-  const moveit::core::JointModelGroup *joint_model_group_gripper =
-      move_group_gripper.getCurrentState()->getJointModelGroup(
-          PLANNING_GROUP_GRIPPER);
-
-  // Get Current State
-  moveit::core::RobotStatePtr current_state_arm =
-      move_group_arm.getCurrentState(10);
-  moveit::core::RobotStatePtr current_state_gripper =
-      move_group_gripper.getCurrentState(10);
-
-  std::vector<double> joint_group_positions_arm;
-  std::vector<double> joint_group_positions_gripper;
-  current_state_arm->copyJointGroupPositions(joint_model_group_arm,
-                                             joint_group_positions_arm);
-  current_state_gripper->copyJointGroupPositions(joint_model_group_gripper,
-                                                 joint_group_positions_gripper);
-
-  move_group_arm.setStartStateToCurrentState();
-  move_group_gripper.setStartStateToCurrentState();
-
-  // Go Home
-  RCLCPP_INFO(LOGGER, "Going Home");
-
-  // joint_group_positions_arm[0] = 0.00;  // Shoulder Pan
-  joint_group_positions_arm[1] = -2.50; // Shoulder Lift
-  joint_group_positions_arm[2] = 1.50;  // Elbow
-  joint_group_positions_arm[3] = -1.50; // Wrist 1
-  joint_group_positions_arm[4] = -1.55; // Wrist 2
-  // joint_group_positions_arm[5] = 0.00;  // Wrist 3
-
-  move_group_arm.setJointValueTarget(joint_group_positions_arm);
-
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan_arm;
-  bool success_arm = (move_group_arm.plan(my_plan_arm) ==
-                      moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_arm.execute(my_plan_arm);
-
-  // Pregrasp
-  RCLCPP_INFO(LOGGER, "Pregrasp Position");
-
-  geometry_msgs::msg::Pose target_pose1;
-  target_pose1.orientation.x = -0.617;
-  target_pose1.orientation.y = 0.787;
-  target_pose1.orientation.z = 0.001;
-  target_pose1.orientation.w = -0.005;
-  target_pose1.position.x = 0.3403;
-  target_pose1.position.y = -0.0193;
-  target_pose1.position.z = 0.28;
-  move_group_arm.setPoseTarget(target_pose1);
-
-  success_arm = (move_group_arm.plan(my_plan_arm) ==
-                 moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_arm.execute(my_plan_arm);
-
-  /*
-  name:
-  - shoulder_pan_joint
-  - shoulder_lift_joint
-  - elbow_joint
-  - wrist_2_joint
-  - wrist_3_joint
-  - wrist_1_joint
-  - robotiq_85_left_knuckle_joint
-  position:
-  - 3.479898861579488
-  - -1.645266757295964
-  - -1.5986352748367567
-  - 1.5691677608597017
-  - -5.704681616196984
-  - -1.4787454182521402
-  - 3.943321953414625e-06
-  */
-
-  // Open Gripper
-
-  RCLCPP_INFO(LOGGER, "Open Gripper!");
-
-  move_group_gripper.setNamedTarget("gripper_open");
-
-  moveit::planning_interface::MoveGroupInterface::Plan my_plan_gripper;
-  bool success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                          moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_gripper.execute(my_plan_gripper);
-
-  // Approach
-  RCLCPP_INFO(LOGGER, "Approach to object!");
-
-  std::vector<geometry_msgs::msg::Pose> approach_waypoints;
-  target_pose1.position.z -= 0.05;
-  approach_waypoints.push_back(target_pose1);
-
-  target_pose1.position.z -= 0.05;
-  approach_waypoints.push_back(target_pose1);
-
-  moveit_msgs::msg::RobotTrajectory trajectory_approach;
-  const double jump_threshold = 0.0;
-  const double eef_step = 0.01;
-
-  double fraction = move_group_arm.computeCartesianPath(
-      approach_waypoints, eef_step, jump_threshold, trajectory_approach);
-
-  move_group_arm.execute(trajectory_approach);
-
-  // Close Gripper
-
-  RCLCPP_INFO(LOGGER, "Close Gripper!");
-
-  move_group_gripper.setNamedTarget("gripper_close");
-
-  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                     moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_gripper.execute(my_plan_gripper);
-
-  // Retreat
-
-  RCLCPP_INFO(LOGGER, "Retreat from object!");
-
-  std::vector<geometry_msgs::msg::Pose> retreat_waypoints;
-  target_pose1.position.z += 0.09;
-  retreat_waypoints.push_back(target_pose1);
-
-  target_pose1.position.z += 0.09;
-  retreat_waypoints.push_back(target_pose1);
-
-  moveit_msgs::msg::RobotTrajectory trajectory_retreat;
-
-  fraction = move_group_arm.computeCartesianPath(
-      retreat_waypoints, eef_step, jump_threshold, trajectory_retreat);
-
-  move_group_arm.execute(trajectory_retreat);
-
-  // Place
-
-  RCLCPP_INFO(LOGGER, "Rotating Arm");
-
-  current_state_arm = move_group_arm.getCurrentState(10);
-  current_state_arm->copyJointGroupPositions(joint_model_group_arm,
-                                             joint_group_positions_arm);
-
-  joint_group_positions_arm[0] = 1.57; // Shoulder Pan
-
-  move_group_arm.setJointValueTarget(joint_group_positions_arm);
-
-  success_arm = (move_group_arm.plan(my_plan_arm) ==
-                 moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_arm.execute(my_plan_arm);
-
-  // Open Gripper
-
-  RCLCPP_INFO(LOGGER, "Release Object!");
-
-  move_group_gripper.setNamedTarget("gripper_open");
-
-  success_gripper = (move_group_gripper.plan(my_plan_gripper) ==
-                     moveit::core::MoveItErrorCode::SUCCESS);
-
-  move_group_gripper.execute(my_plan_gripper);
-
-  rclcpp::shutdown();
-  return 0;
+    rclcpp::shutdown();
+    return 0;
 }
